eb09308ec636414a35052d23dc04ec29400aa23a
Leandro Siqueira <leandrosiqueira@outlook.com>
2022-12-28 10:47:51 -0300

    Auto-generated commit [ci skip]

--- src/app/interfaces/problema.ts
+++
@@ -1,8 +0,0 @@
-export interface Problema {
-  idProblema: number;
-  tituloProblema: string;
-  descricaoProblema: string;
-  prioridadeProblema: 'baixa' | 'alta';
-  tipoProblema: 'Recurso' | 'Erro' | 'Documentação';
-  dataCorrecaoProblema?: Date;
-}
